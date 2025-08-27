b865d80c01330e2308db94b7a58ca7656a8971b7
Zoe Aspenns <aspennza@mail.uc.edu>
2025-08-27 12:42:07 -0400

    Added a loop for taking user input into the record variables; created a personRec String for holding all the data

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+//Need to write javadoc later!
 
 public class PersonGenerator
 {
     public static void main(String[] args)
     {
-        ArrayList<String> people = new ArrayList<String>();
+        ArrayList<String> people = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
 
         //here go scanner, variables, arraylist, etc.
+        String personRec = "";
         String ID = "";
         String firstName = "";
         String lastName = "";
@@ -14,7 +20,17 @@ public class PersonGenerator
         int YOB = 0;
 
 
-        //open a loop i think?
+        //open a loop I think?
+
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Please enter the ID for your record [6 digits]");
+            firstName = SafeInput.getNonZeroLenString(in, "Please enter the first name for your record");
+            lastName = SafeInput.getNonZeroLenString(in, "Please enter the last name for your record");
+            title = SafeInput.getNonZeroLenString(in, "Please enter the title for your record");
+            YOB = SafeInput.getInt(in, "Please enter the year of birth for your record");
+
+        }while(!done);
+
 
         //prompt the user to enter data
         //put data into arraylist

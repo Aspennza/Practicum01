8c3f597e14996079ad1cd1f452789e15fb742671
Zoe Aspenns <aspennza@mail.uc.edu>
2025-08-27 12:15:23 -0400

    Added variables to hold person data and ArrayList to hold person data

---
+++ src/PersonGenerator.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> people = new ArrayList<String>();
+
+        //here go scanner, variables, arraylist, etc.
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        int YOB = 0;
+
+
+        //open a loop i think?
+
+        //prompt the user to enter data
+        //put data into arraylist
+
+        //ask the user if they want to enter more records
+
+        //create writer
+        //open a try catch around the writer
+        //create the writer and all the filler code for that
+
+
+
+    }
+}

43c6e651707185a053ca1606e6fad4dae36c0743
Zoe Aspenns <aspennza@mail.uc.edu>
2025-08-27 22:01:40 -0400

    ProductReader.java created; imported NIO components & created variables

---
+++ src/ProductReader.java
@@ -0,0 +1,37 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import static java.lang.System.out;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import static java.nio.file.StandardOpenOption.CREATE;
+import javax.swing.JFileChooser;
+
+public class ProductReader
+{
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> products = new ArrayList<>();
+
+        final int FIELDS_LENGTH = 4;
+        String ID = "";
+        String name = "";
+        String description = "";
+        double cost = 0.00;
+
+
+    }
+}

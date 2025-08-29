8f91556d7b3541ce9c03e61e65fc940c04d8b92e
Zoe Aspenns <aspennza@mail.uc.edu>
2025-08-29 09:28:40 -0400

    PersonGenerator.java documentation updated

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -9,64 +9,45 @@ import static java.nio.file.StandardOpenOption.CREATE;
 
 public class PersonGenerator
 {
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
-        /**
-         * This ArrayList holds a list of all person records input by the user
-         */
+         //This ArrayList holds a list of all person records input by the user
         ArrayList<String> people = new ArrayList<>();
-        /**
-         * This Scanner takes all user input
-         */
+
+         //This Scanner takes all user input
         Scanner in = new Scanner(System.in);
 
-        /**
-         * This file holds the current directory
-         */
+         //This file holds the current directory
         File workingDirectory = new File(System.getProperty("user.dir"));
-        /**
-         * This Path holds the current directory concatenated with the location of the text file that will be created
-         */
+
+         //This Path holds the current directory concatenated with the location of the text file that will be created
         Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
 
-        /**
-         * This boolean holds the true/false value that determines if the user continues or stops entering records
-         */
+         //This boolean holds the true/false value that determines if the user continues or stops entering records
         boolean done = false;
 
-        /**
-         * This String holds the finished comma-separated list of each person's ID, firstName, lastName, title, and YOB
-         */
+         //This String holds the finished comma-separated list of each person's ID, firstName, lastName, title, and YOB
         String personRec = "";
 
-        /**
-         * This String holds the 6-digit ID of each person record input by the user
-         */
+         //This String holds the 6-digit ID of each person record input by the user
         String ID = "";
 
-        /**
-         * This String holds the first name of each person input by the user
-         */
+         //This String holds the first name of each person input by the user
         String firstName = "";
 
-        /**
-         * This String holds the last name of each person input by the user
-         */
+         //This String holds the last name of each person input by the user
         String lastName = "";
 
-        /**
-         * This String holds the title (Mr., Ms., Esq., etc.) of each person input by the user
-         */
+         //This String holds the title (Mr., Ms., Esq., etc.) of each person input by the user
         String title = "";
 
-        /**
-         * This int holds the birth year of each person input by the user
-         */
+         //This int holds the birth year of each person input by the user
         int YOB = 0;
 
-        /**
-         * This algorithm prompts the user to enter an ID, firstName, lastName, title, and YOB, concatenates these values into a record, adds the record to the people ArrayList, then determines if the user is finished entering records
-         */
+         //This algorithm prompts the user to enter an ID, firstName, lastName, title, and YOB, concatenates these values into a record, adds the record to the people ArrayList, then determines if the user is finished entering records
         do {
             ID = SafeInput.getNonZeroLenString(in, "Please enter the ID for your record [6 digits]");
             firstName = SafeInput.getNonZeroLenString(in, "Please enter the first name for your record");
@@ -80,17 +61,13 @@ public class PersonGenerator
             done = SafeInput.getYNConfirm(in, "Have you finished entering records?");
         }while(!done);
 
-        /**
-         * This algorithm prints each record in the people ArrayList to the console
-         */
+         //This algorithm prints each record in the people ArrayList to the console
         for (String p : people)
         {
             System.out.println(p);
         }
 
-        /**
-         * This algorithm writes each record in the people ArrayList into the PersonTestData.txt file indicated above, then checks for exceptions
-         */
+         //This algorithm writes each record in the people ArrayList into the PersonTestData.txt file indicated above, then checks for exceptions
         try
         {
             OutputStream out =
@@ -98,6 +75,7 @@ public class PersonGenerator
             BufferedWriter writer =
                     new BufferedWriter(new OutputStreamWriter(out));
 
+             //This algorithm writes each record in the people ArrayList into the PersonTestData.txt file
             for(String rec : people)
             {
                 writer.write(rec, 0, rec.length());
